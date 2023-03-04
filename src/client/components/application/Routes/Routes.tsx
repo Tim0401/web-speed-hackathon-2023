f52dfa32dd0a49a97c9948b99d95af5bf87f928e
Tim0401 <36380433+Tim0401@users.noreply.github.com>
2023-03-05 02:47:04 +0900

    use lazy load

--- src/client/components/application/Routes/Routes.tsx
+++ src/client/components/application/Routes/Routes.tsx
@@ -1,13 +1,14 @@
 import type { FC } from 'react';
+import { lazy } from 'react';
 import * as Router from 'react-router-dom';
 
-import { NotFound } from '../../../pages/NotFound';
-import { Order } from '../../../pages/Order';
-import { OrderComplete } from '../../../pages/OrderComplete';
-import { ProductDetail } from '../../../pages/ProductDetail';
-import { Top } from '../../../pages/Top';
-
 import { useScrollToTop } from './hooks';
+
+const Top = lazy(() => import('../../../pages/Top').then((module) => ({ default: module.Top })));
+const ProductDetail = lazy(() => import('../../../pages/ProductDetail').then((module) => ({ default: module.ProductDetail })));
+const OrderComplete = lazy(() => import('../../../pages/OrderComplete').then((module) => ({ default: module.OrderComplete })));
+const Order = lazy(() => import('../../../pages/Order').then((module) => ({ default: module.Order })));
+const NotFound = lazy(() => import('../../../pages/NotFound').then((module) => ({ default: module.NotFound })));
 
 export const Routes: FC = () => {
   useScrollToTop();
